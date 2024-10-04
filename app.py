0328d3db6d21645d5953bff2b351ef8a294f796a
CDepontieu <97032871+CDepontieu@users.noreply.github.com>
2024-10-04 14:31:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ authenticator = stauth.Authenticate(
     #config['pre-authorized']
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
+name, authentication_status, username = authenticator.login('Login', 'sidebar')
 
 #authenticator.login()
 
