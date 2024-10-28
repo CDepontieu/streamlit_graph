97013618006784bc088ce76830a080349ccdc83c
CDepontieu <97032871+CDepontieu@users.noreply.github.com>
2024-10-28 16:16:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -142,18 +142,17 @@ if st.session_state['authentication_status']:
         
     st.dataframe(filtered_df)        
 
-
-      
-    Htmlfile = got.simple_func(filtered_df, hierachical_display, height_input)
-
-    Htmlfile = got.add_function_htmlTitle(Htmlfile)
-    Htmlfile = got.use_htmlTitle(Htmlfile)
-    Htmlfile = got.add_div_display_info_node(Htmlfile)
-    Htmlfile = got.add_onclick_node_event(Htmlfile)
-
-    #HtmlFile = open("test.html", 'r')#, encoding='utf-8')
-    #source_code = Htmlfile.read() 
-    components.html(Htmlfile, height = int(height_input)*1.6,width=width_input_px, scrolling=True)
+    if st.button("Submit_dataframe"):
+        Htmlfile = got.simple_func(filtered_df, hierachical_display, height_input)
+    
+        Htmlfile = got.add_function_htmlTitle(Htmlfile)
+        Htmlfile = got.use_htmlTitle(Htmlfile)
+        Htmlfile = got.add_div_display_info_node(Htmlfile)
+        Htmlfile = got.add_onclick_node_event(Htmlfile)
+    
+        #HtmlFile = open("test.html", 'r')#, encoding='utf-8')
+        #source_code = Htmlfile.read() 
+        components.html(Htmlfile, height = int(height_input)*1.6,width=width_input_px, scrolling=True)
 
 
   #got.got_func(physics)
